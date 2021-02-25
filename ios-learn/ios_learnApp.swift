f1f7536ae05ecfba1fa3e7c6c2617746a59ef608
taylenxu <taylenxu@tencent.com>
2021-02-25 12:22:41 +0800

    Initial Commit

---
+++ ios-learn/ios_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_learnApp.swift
+//  ios-learn
+//
+//  Created by 许士涛 on 2021/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
